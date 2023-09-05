6605c186ff9ca93ba37f92335f99b9048cd7602b
MaksymSkala <maksymskala@gmail.com>
2023-09-06 00:41:06 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import SimpleLightbox from 'simplelightbox';
-
+import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
