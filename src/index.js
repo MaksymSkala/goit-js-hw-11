1ada5783d762285da86ecb1c221b6f990f27dd11
MaksymSkala <maksymskala@gmail.com>
2023-09-06 00:23:22 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,88 +1,130 @@
-const BASE_URL = 'https://pixabay.com/api/';
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+
+const form = document.getElementById('search-form');
+const gallery = document.querySelector('.gallery');
+const loadMoreButton = document.querySelector('.load-more');
+
 const API_KEY = '39263242-9a9e6029252b757da1c0dfd4b';
-const perPage = 20; // Кількість зображень на сторінці
+const BASE_URL = 'https://pixabay.com/api/';
+
+const lightbox = new SimpleLightbox('.gallery a');
 
 let page = 1; // Початкова сторінка
+let currentQuery = ''; // Поточний запит
 
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('.load-more');
+function clearGallery() {
+    gallery.innerHTML = '';
+}
 
-// Функція для виконання HTTP-запиту до Pixabay API
-async function fetchImages(searchQuery) {
-  try {
-    const response = await fetch(
-      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-    );
-    if (!response.ok) {
-      throw new Error('Error fetching images');
+async function fetchImages(query, pageNum) {
+    try {
+        const response = await axios.get(BASE_URL, {
+            params: {
+                key: API_KEY,
+                q: query,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page: pageNum,
+                per_page: 40,
+            },
+        });
+
+        const { data } = response;
+        const { hits, totalHits } = data;
+
+        if (hits.length === 0) {
+            // Відобразити повідомлення, що нічого не знайдено
+            return;
+        }
+
+        // Рендерити кожну картку зображення
+        hits.forEach((image) => {
+            const photoCard = document.createElement('div');
+            photoCard.classList.add('photo-card');
+
+            const img = document.createElement('img');
+            img.src = image.webformatURL;
+            img.alt = image.tags;
+            img.loading = 'lazy';
+
+            const info = document.createElement('div');
+            info.classList.add('info');
+
+            const likes = document.createElement('p');
+            likes.classList.add('info-item');
+            likes.innerHTML = `<b>Likes:</b> ${image.likes}`;
+
+            const views = document.createElement('p');
+            views.classList.add('info-item');
+            views.innerHTML = `<b>Views:</b> ${image.views}`;
+
+            const comments = document.createElement('p');
+            comments.classList.add('info-item');
+            comments.innerHTML = `<b>Comments:</b> ${image.comments}`;
+
+            const downloads = document.createElement('p');
+            downloads.classList.add('info-item');
+            downloads.innerHTML = `<b>Downloads:</b> ${image.downloads}`;
+
+            info.appendChild(likes);
+            info.appendChild(views);
+            info.appendChild(comments);
+            info.appendChild(downloads);
+
+            photoCard.appendChild(img);
+            photoCard.appendChild(info);
+
+            gallery.appendChild(photoCard);
+        });
+
+        // Відобразити повідомлення про кількість знайдених зображень
+        alert(`Hooray! We found ${totalHits} images.`);
+
+        // Включити кнопку "Load more"
+        loadMoreButton.style.display = 'block';
+    } catch (error) {
+        console.error('Error fetching images:', error);
     }
-    const data = await response.json();
-    return data.hits;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    return [];
-  }
 }
 
-// Функція для додавання карток зображень до галереї
-function appendImagesToGallery(images) {
-  const galleryItemsHTML = images
-    .map(
-      (item) =>
-        `<div class="photo-card">
-           <img src="${item.largeImageURL}" alt="${item.tags}" loading="lazy" />
-           <div class="info">
-             <p class="info-item"><b>Likes:</b> ${item.likes}</p>
-             <p class="info-item"><b>Views:</b> ${item.views}</p>
-             <p class="info-item"><b>Comments:</b> ${item.comments}</p>
-             <p class="info-item"><b>Downloads:</b> ${item.downloads}</p>
-           </div>
-         </div>`
-    )
-    .join('');
+function searchImages(query) {
+    if (query.trim() === '') {
+        return;
+    }
 
-  gallery.insertAdjacentHTML('beforeend', galleryItemsHTML);
+    // Очистити галерею при новому пошуку
+    clearGallery();
+
+    // Почати з першої сторінки
+    page = 1;
+
+    // Запам'ятати поточний запит
+    currentQuery = query;
+
+    // Виконати HTTP-запит та рендерити зображення
+    fetchImages(query, page);
 }
 
-// Функція для обробки подальших запитів при натисканні кнопки "Load more"
-async function handleLoadMore() {
-  page += 1; // Збільшуємо номер сторінки
-  const searchQuery = document.querySelector('input[name="searchQuery"]').value;
-  const images = await fetchImages(searchQuery);
+function loadMoreImages() {
+    // Збільшити номер сторінки на 1
+    page++;
 
-  if (images.length === 0) {
-    // Перевірка на порожній масив результатів
-    loadMoreBtn.style.display = 'none'; // Якщо немає результатів, ховаємо кнопку "Load more"
-    console.log("Sorry, there are no more images matching your search query.");
-  } else {
-    appendImagesToGallery(images);
-    window.scrollBy({
-      top: gallery.clientHeight * 2,
-      behavior: 'smooth',
-    });
-  }
+    // Виконати HTTP-запит та додати наступну групу зображень до галереї
+    fetchImages(currentQuery, page);
 }
 
-document.querySelector('form').addEventListener('submit', async (e) => {
-  e.preventDefault();
-  page = 1; // Скидаємо номер сторінки при новому пошуку
-  gallery.innerHTML = ''; // Очищуємо галерею
-  loadMoreBtn.style.display = 'none'; // Ховаємо кнопку "Load more"
-
-  const searchQuery = e.target.searchQuery.value;
-  const images = await fetchImages(searchQuery);
-
-  if (images.length === 0) {
-    console.log("Sorry, there are no images matching your search query. Please try again.");
-    return;
-  }
-
-  appendImagesToGallery(images);
-  loadMoreBtn.style.display = 'block'; // Показуємо кнопку "Load more"
-  window.scrollBy({
-    top: gallery.clientHeight,
-    behavior: 'smooth',
-  });
+// Обробники подій
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const query = e.target.searchQuery.value;
+    searchImages(query);
 });
 
-loadMoreBtn.addEventListener('click', handleLoadMore);
+loadMoreButton.addEventListener('click', loadMoreImages);
+
+// Пошук при завантаженні сторінки
+window.addEventListener('load', () => {
+    searchImages('landscape'); // Приклад пошуку за замовчуванням
+});
